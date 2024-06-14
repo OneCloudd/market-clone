ab7978900d7792d25c8d393f72d2a4e144803cfb
OneCloudd <whdlfdns12@gmail.com>
2024-06-14 14:10:53 +0900

    유저인증까지 Firebase로 구

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -1,6 +1,39 @@
 <script>
+  import { onMount } from "svelte";
+  import Nav from "../components/Nav.svelte";
+  import { getDatabase, ref, onValue } from "firebase/database";
+
   let hour = new Date().getHours();
   let min = new Date().getMinutes();
+
+  //   반응형 변수 선언
+  $: items = [];
+
+  //DB에서 데이터 읽어오기
+  const db = getDatabase();
+  const itemsRef = ref(db, "items/");
+
+  //onMount를 써서 화면이 랜더링 될 때 마다 출력하게 함
+  onMount(() => {
+    onValue(itemsRef, (snapshot) => {
+      const data = snapshot.val();
+      items = Object.values(data).reverse();
+    });
+  });
+
+  const calcTime = (timestamp) => {
+    // 한국시간 UTC+9 로 맞추기 위해 9*60*60*1000 값을 빼줌
+    const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
+    const time = new Date(curTime - timestamp);
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
+
+    if (hour > 0) return `${hour}시간 전`;
+    else if (minute > 0) return `${minute}분 전`;
+    else if (second > 0) return `${second}초 전`;
+    else return "방금전";
+  };
 </script>
 
 <header>
@@ -16,53 +49,35 @@
     <div class="menu-bar__location">
       <div>역삼1동</div>
       <div class="menu-bar__location-icon">
-        <img src="assets/arrow.svg" />
+        <img src="assets/arrow.svg" alt="" />
       </div>
     </div>
     <div class="menu-bar__icons">
-      <img src="assets/search.svg" />
-      <img src="assets/menu.svg" />
-      <img src="assets/alert.svg" />
+      <img src="assets/search.svg" alt="" />
+      <img src="assets/menu.svg" alt="" />
+      <img src="assets/alert.svg" alt="" />
     </div>
   </div>
 </header>
 
 <main>
+  {#each items as item}
+    <div class="item-list">
+      <div class="item-list__img">
+        <img src={item.imgUrl} alt="" />
+      </div>
+      <div class="item-list__info">
+        <div class="item-list__info-title">{item.title}</div>
+        <div class="item-list__info-meta">
+          {item.place}
+          {calcTime(item.insertAt)}
+        </div>
+        <div class="item-list__info-price">{item.price}</div>
+        <div class="item-list__info-description">{item.description}</div>
+      </div>
+    </div>
+  {/each}
   <a class="write-btn" href="#/write">+ 글쓰기</a>
 </main>
-
-<footer>
-  <div class="footer-block">
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/home.svg" />
-      </div>
-      <div class="footer-icons__desc">홈</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/doc.svg" />
-      </div>
-      <div class="footer-icons__desc">동네생활</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/location.svg" />
-      </div>
-      <div class="footer-icons__desc">내 근처</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/chat.svg" />
-      </div>
-      <div class="footer-icons__desc">채팅</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/user.svg" />
-      </div>
-      <div class="footer-icons__desc">나의 당근</div>
-    </div>
-  </div>
-</footer>
+<Nav location="home" />
 <div class="media-info-msg">화면 사이즈를 줄여주세요.</div>
